9b9572cd68d79e8c8db649dc2389df25a8fcf8d2
Kedtol <morakedtol@gmail.com>
2019-06-29 20:27:47 +0200

    Initial build

--- src/Map.java
+++ src/Map.java
@@ -41,7 +41,6 @@ public class Map
 		private int item_width = 32;
 		public int screen_px = 200;
 		public int screen_py = 200;
-		private Player player;
 		
 		private void drawItem(Graphics g, int x, int y, boolean top)
 		{
@@ -52,14 +51,14 @@ public class Map
 			if (top == true)
 			{
 				g.setColor(Color.RED);
-			x_calc_1 = x+Math.sin(Math.toRadians((360/angle)+45))*item_width;
-			y_calc_1 = y+Math.cos(Math.toRadians((360/angle)+45))*item_width;
+				x_calc_1 = x+Math.sin(Math.toRadians((360/angle)+45))*item_width;
+				y_calc_1 = y+Math.cos(Math.toRadians((360/angle)+45))*item_width;
 			}
 			else
 			{
 				g.setColor(Color.GREEN);
-			x_calc_1 = x+Math.sin(Math.toRadians((360/angle*2)+45))*item_width;
-			y_calc_1 = y+Math.cos(Math.toRadians((360/angle*2)+45))*item_width;
+				x_calc_1 = x+Math.sin(Math.toRadians((360/angle*2)+45))*item_width;
+				y_calc_1 = y+Math.cos(Math.toRadians((360/angle*2)+45))*item_width;
 			}
 			
 			for (int i = 2; i <= angle+1; i++)
@@ -123,7 +122,7 @@ public class Map
 			}
 		}
 		
-		public void drawMapOrder(Graphics g, Texture SpriteSheet, Camera camera)
+		public void drawMapOrder(Graphics g, Texture SpriteSheet, Camera camera, boolean alpha)
 		{
 			
 			int camX = camera.x;
@@ -153,7 +152,8 @@ public class Map
 					cell[0] = -1;
 					cell[1] = -2;
 					cell[2] = camera.z;
-					
+
+
 				break;
 				
 				case 1:
@@ -178,23 +178,29 @@ public class Map
 				
 				}
 			
-			int iterator;
-			int iterator1;
-			
 			for (int i = camX-camX_rs-1; i < camX+camX_rs; i++)
 			{
 				for (int i1 = camY-camY_rs-1; i1 < camY+camY_rs; i1++)
 				{
-					
-					cellR = cellSet(i,i1,cell);
-					drawMap(g,SpriteSheet,cellR[0],cellR[1],cellR[2],i,i1,camX,camY,camX_rs,camY_rs,camera);
-
+					if (alpha == true)
+					{
+						for (int i2 = -1; i2 <= 1; i2++)
+						{
+							cellR = cellSet(i, i1, cell,i2);
+							drawMap(g, SpriteSheet, cellR[0], cellR[1], cellR[2], i, i1, camX, camY, camX_rs, camY_rs, camera,true,i2);
+						}
+					}
+					else
+					{
+						cellR = cellSet(i, i1, cell,0);
+						drawMap(g, SpriteSheet, cellR[0], cellR[1], cellR[2], i, i1, camX, camY, camX_rs, camY_rs, camera,false,0);
+					}
 				}
 				
 			}
 		}
 		
-		private int[] cellSet(int iterator,int iterator1,int[] _cell)
+		private int[] cellSet(int iterator,int iterator1,int[] _cell,int iterator2)
 		{
 			int[] cell_;
 			cell_ = new int[3];
@@ -213,22 +219,36 @@ public class Map
 					}
 					else
 					{
-						cell_[i2] = _cell[i2];
+						cell_[i2] = _cell[i2]+iterator2;
 					}
 				}
 			}
 			return cell_;
 		}
 		
-		private void drawMap(Graphics g, Texture SpriteSheet, int cellX, int cellY, int cellZ, int cam_i, int cam_i1, int camX, int camY, int camX_rs, int camY_rs, Camera camera)
+		private void drawMap(Graphics g, Texture SpriteSheet, int cellX, int cellY, int cellZ, int cam_i, int cam_i1, int camX, int camY, int camX_rs, int camY_rs, Camera camera,boolean alpha,int cam_i2)
 		{
 			double item_width_c = Math.sqrt(Math.pow(item_width,2)+Math.pow(item_width,2))+2;
-			int padding_camera_x = cam_i-(camX-camX_rs);
-			int padding_camera_y = cam_i1-(camY-camY_rs);
+			float padding_camera_x = cam_i-(camX-camX_rs);
+			float padding_camera_y = cam_i1-(camY-camY_rs);
+			float alphaValue = 1f;
+
+			if (alpha == true)
+			{
+
+				int padding_camera_z = 1 * cam_i2;
+
+				if (cam_i2 != 0)
+				{
+					padding_camera_x = padding_camera_x + padding_camera_z / 1.5f;
+					padding_camera_y = padding_camera_y + padding_camera_z / 1.5f;
+					alphaValue = 0.5f;
+				}
+			}
 
 			if (map[cellX][cellY][cellZ].hasBomb == true)
 			{
-				drawItemTexture(g,screen_px+padding_camera_x*((int) item_width_c)+camera.drawPadding,screen_py+padding_camera_y *((int) item_width_c),5,SpriteSheet,1f);
+				drawItemTexture(g,screen_px+(int)(padding_camera_x*item_width_c)+camera.drawPadding,screen_py+(int)(padding_camera_y * item_width_c),5,SpriteSheet,alphaValue);
 			}
 
 			for (int i = 0; i < maxPlayers; i++)
@@ -237,12 +257,12 @@ public class Map
 
 				if (cellX == iPlayer.x && cellY == iPlayer.y && cellZ == iPlayer.z) {
 					if (iPlayer.alive == true) {
-						drawItemTexture(g, screen_px + padding_camera_x * ((int) item_width_c) + camera.drawPadding, screen_py + padding_camera_y * ((int) item_width_c), 9, SpriteSheet, 1f);
+						drawItemTexture(g,screen_px+(int)(padding_camera_x*item_width_c)+camera.drawPadding,screen_py+(int)(padding_camera_y * item_width_c),9, SpriteSheet, alphaValue);
 					}
 				}
 			}
-			
-			drawItemTexture(g,screen_px+padding_camera_x*((int) item_width_c)+camera.drawPadding,screen_py+padding_camera_y *((int) item_width_c),map[cellX][cellY][cellZ].contain,SpriteSheet,1f);
+			drawItemTexture(g,screen_px+(int)(padding_camera_x*item_width_c)+camera.drawPadding,screen_py+(int)(padding_camera_y * item_width_c),map[cellX][cellY][cellZ].contain,SpriteSheet,alphaValue);
+			//drawItemTexture(g,screen_px+padding_camera_x*((int) item_width_c)+camera.drawPadding,screen_py+padding_camera_y *((int) item_width_c),map[cellX][cellY][cellZ].contain,SpriteSheet,alphaValue);
 		}
 		
 		public void drawMapAlpha(Graphics g, Texture SpriteSheet, Camera camera)
@@ -500,11 +520,11 @@ public class Map
 
 			if (_mode == 0)
 			{
-				iPlayer.camera.drawMapAlpha(g,_texture,iPlayer.camera);
+				iPlayer.camera.drawMapOrder(g, _texture,iPlayer.camera,true);
 			}
 			else
 			{
-				iPlayer.camera.drawMapOrder(g, _texture,iPlayer.camera);
+				iPlayer.camera.drawMapOrder(g, _texture,iPlayer.camera,false);
 			}
 
 		}

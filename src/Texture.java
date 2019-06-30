40a3676f6164769c111084d157c58d52cdc452ba
Kedtol <morakedtol@gmail.com>
2019-07-01 00:07:22 +0200

    - DataTags - todo: PowerUps

--- src/Texture.java
+++ src/Texture.java
@@ -2,36 +2,37 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
+import javax.imageio.*;
 
 public class Texture
 	{
 		private int spriteAmount = 10;
 		private int dataTagAmount = 10;
 		private boolean hasDataTag = false;
-		public BufferedImage[][] SpriteSheet = new BufferedImage[spriteAmount][dataTagAmount];
-		
+		public BufferedImage[] SpriteSheet = new BufferedImage[spriteAmount];//[dataTagAmount];
 		public Texture()
 		{
 			
 			for(int i = 0; i < spriteAmount;i++)
 			{
 				try
-			    {
-					SpriteSheet[i][0] = ImageIO.read(getClass().getResource("SpriteSheet"+i+".png"));
-			    }
-			    catch (IOException e)
-			    {
-			    	//empty
-			    	SpriteSheet[i][0] = null;
-			    }
-
-				if (SpriteSheet[i][0] == null)
 				{
-					try
+					SpriteSheet[i] = ImageIO.read(getClass().getResource("SpriteSheet"+String.valueOf(i)+".png"));
+
+				}
+				catch (IOException e)
+				{
+					//No image.
+					System.out.println("Image missing: "+String.valueOf(i));
+					SpriteSheet[i] = null;
+				}
+
+				if (SpriteSheet[i] == null)
+				{
+					/*try
 					{
 						hasDataTag = true;
-						SpriteSheet[i][0] = ImageIO.read(getClass().getResource("SpriteSheet"+i+"_0.png"));
+						//SpriteSheet[i][0] = ImageIO.read(getClass().getResource("SpriteSheet"+i+"_0.png"));
 
 					}
 					catch (IOException e)
@@ -39,10 +40,10 @@ public class Texture
 						System.out.println("Image missing: "+String.valueOf(i));
 						SpriteSheet[i][0] = null;
 						hasDataTag = false;
-					}
+					}*/
 				}
 
-				if (SpriteSheet[i][0] != null && hasDataTag == true)
+				/*if (SpriteSheet[i][0] != null && hasDataTag == true)
 				{
 					for(int i1 = 0; i1 < dataTagAmount;i1++)
 					{
@@ -60,7 +61,7 @@ public class Texture
 							hasDataTag = false;
 						}
 					}
-				}
+				}*/
 
 
 			}

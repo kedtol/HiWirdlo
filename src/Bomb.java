509e1353b7899db5e2fc9874f4ac43ce61888146
kedtol <morakedtol@gmail.com>
2019-08-06 19:38:14 +0200

    - Bomb push (once again)

--- src/Bomb.java
+++ src/Bomb.java
@@ -4,15 +4,17 @@ public class Bomb
 	public int x;
 	public int y;
 	public int z;
-	private int created;
+	private int spawnTick;
 	public int size;
 	private int[] explosionIndex = new int[6];
 	private boolean explosion;
 	public Player iPlayer;
 	int bombDirection = 4;
+	public boolean move;
+	public int direction = 0;
 
 	private Alarm alarm = new Alarm();
-
+	private Alarm movementAlarm = new Alarm();
 	public Bomb()
 	{
 		x = 0;
@@ -27,13 +29,13 @@ public class Bomb
 		explosionIndex[5] = 0;
 
 		size = 3;
-		created = Time.fullTick;
+		spawnTick = Time.fullTick;
 
 		alarm.setLength(5);
 		alarm.start();
 
 		explosion = true;
-
+		movementAlarm.setLength(0.2f);
 	}
 
 	public void explode()
@@ -48,16 +50,8 @@ public class Bomb
 				{
 					if (explosionIndex[0] == 0)
 					{
-						//if (collide(0,0,0) == 1)
-						//{
 						setExplosion(0,0);
-						/*}
-						else
-						{
-							System.out.println("systemTick - bomb disarmed");
-							iPlayer.bombCount -= 1;
-							map[x][y][z].hasBomb = false;
-						}*/
+
 						explosionIndex[0] = 1;
 						explosionIndex[1] = 1;
 						explosionIndex[2] = 1;
@@ -185,43 +179,43 @@ public class Bomb
 			case 2: //up
 				map[x][y-speed][z].id = 3;
 				map[x][y-speed][z].dataTag = 0;
-				map[x][y-speed][z].planted = created;
+				map[x][y-speed][z].planted = spawnTick;
 				break;
 
 			case 4: //down
 				map[x][y+speed][z].id = 3;
 				map[x][y+speed][z].dataTag = 0;
-				map[x][y+speed][z].planted = created;
+				map[x][y+speed][z].planted = spawnTick;
 				break;
 
 			case 3: //left
 				map[x-speed][y][z].id = 3;
 				map[x-speed][y][z].dataTag = 1;
-				map[x-speed][y][z].planted = created;
+				map[x-speed][y][z].planted = spawnTick;
 				break;
 
 			case 1: //right
 				map[x+speed][y][z].id = 3;
 				map[x+speed][y][z].dataTag = 1;
-				map[x+speed][y][z].planted = created;
+				map[x+speed][y][z].planted = spawnTick;
 				break;
 
 			case 5: //z up
 				map[x][y][z-speed].id = 3;
 				map[x][y][z-speed].dataTag = 3;
-				map[x][y][z-speed].planted = created;
+				map[x][y][z-speed].planted = spawnTick;
 				break;
 
 			case 6: //z down
 				map[x][y][z+speed].id = 3;
 				map[x][y][z+speed].dataTag = 3;
-				map[x][y][z+speed].planted = created;
+				map[x][y][z+speed].planted = spawnTick;
 				break;
 
 			case 0: //none
 				map[x][y][z].id = 3;
 				map[x][y][z].dataTag = 2;
-				map[x][y][z].planted = created;
+				map[x][y][z].planted = spawnTick;
 				break;
 		}
 	}
@@ -410,5 +404,121 @@ public class Bomb
 				break;
 		}
 		return 0;
+	}
+
+	public void moveForward()
+	{
+		if (move == true)
+		{
+			if (direction > 0 && collide(direction,1,1) == 0 && collide(direction,1,5) == 0 && collide(direction,1,2) == 0 && collide(direction,1,4) == 0) //speed kell majd későbbiekben
+			{
+				if (movementAlarm.finished() == true)
+				{
+					int x_s = x;
+					int y_s = y;
+					int z_s = z;
+
+					Map.Item myItem = Map.getItem(x,y,z,direction,1);
+					moveDirection(direction);
+					myItem.hasBomb = true;
+					myItem.bomb = this;
+					myItem.bomb.move = true;
+					myItem.id = 5;
+					Map.setItem(x_s,y_s,z_s,direction,1,myItem);
+					map[x_s][y_s][z_s].hasBomb = false;
+					map[x_s][y_s][z_s].id = 0;
+
+					movementAlarm.start();
+				}
+			}
+			else
+			{
+				move = false;
+			}
+		}
+	}
+
+	private void moveDirection(int direction)
+	{
+		switch(direction)
+		{
+			case 2:
+
+				if (y-1 > 0)
+				{
+					y -= 1;
+				}
+				else
+				{
+					y = 0;
+				}
+
+				break;
+
+			case 4:
+
+				if (y+1 < Map.length)
+				{
+					y += 1;
+				}
+				else
+				{
+					y = Map.length;
+				}
+
+				break;
+
+			case 3:
+
+				if (x-1 > 0)
+				{
+					x -= 1;
+				}
+				else
+				{
+					x = 0;
+				}
+
+				break;
+
+			case 1:
+
+				if (x+1 < Map.length)
+				{
+					x += 1;
+				}
+				else
+				{
+					x = Map.length;
+				}
+
+				break;
+
+			case 5:
+
+				if (z-1 > 0)
+				{
+					z -= 1;
+				}
+				else
+				{
+					z = 0;
+				}
+
+				break;
+
+			case 6:
+
+				if (z+1 < Map.length_z-1)
+				{
+					z += 1;
+				}
+				else
+				{
+					z = Map.length_z-1;
+				}
+
+				break;
+		}
 	}
 }

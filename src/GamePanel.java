5f3727d08d0282715f4cf8734b559afd093f6a26
kedtol <morakedtol@gmail.com>
2019-08-22 18:47:41 +0200

    -"Network loop"

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
@@ -140,7 +141,7 @@ public class GamePanel extends JPanel
         }
     }
 	
-	public void Loop() throws InterruptedException
+	public void Loop() throws InterruptedException, IOException, ClassNotFoundException
 	{
 		while(true)
 		{
@@ -149,9 +150,12 @@ public class GamePanel extends JPanel
 
 			if (visible == true)
 			{
+
 				game.serverState = server;
 				game.clientState = client;
 				game.ip = ip;
+				game.networkCode();
+				System.out.println("no");
 			}
 
 			Thread.sleep(16);
